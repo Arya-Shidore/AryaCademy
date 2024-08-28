c771e81356ad4f76862728fb76651e31ef461182
Arya-Shidore <aryashidore2002@gmail.com>
2024-08-28 23:13:02 +0530

    fixed env files

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,13 +2,22 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCdeuZfQjuwDkpxKrIL7ddEpo-_QJCDHAI",
+//   authDomain: "aryacode-61096.firebaseapp.com",
+//   projectId: "aryacode-61096",
+//   storageBucket: "aryacode-61096.appspot.com",
+//   messagingSenderId: "474764157916",
+//   appId: "1:474764157916:web:95d1d39577156c571da519"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCdeuZfQjuwDkpxKrIL7ddEpo-_QJCDHAI",
-  authDomain: "aryacode-61096.firebaseapp.com",
-  projectId: "aryacode-61096",
-  storageBucket: "aryacode-61096.appspot.com",
-  messagingSenderId: "474764157916",
-  appId: "1:474764157916:web:95d1d39577156c571da519"
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();

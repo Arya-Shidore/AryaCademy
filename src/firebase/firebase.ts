e262ab5dbcb915369657fa98641e058c9f59ebbc
Arya-Shidore <aryashidore2002@gmail.com>
2024-08-29 08:13:03 +0530

    en file edits

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,15 +2,6 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCdeuZfQjuwDkpxKrIL7ddEpo-_QJCDHAI",
-//   authDomain: "aryacode-61096.firebaseapp.com",
-//   projectId: "aryacode-61096",
-//   storageBucket: "aryacode-61096.appspot.com",
-//   messagingSenderId: "474764157916",
-//   appId: "1:474764157916:web:95d1d39577156c571da519"
-// };
-
 const firebaseConfig = {
 	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
 	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
